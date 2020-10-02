bbf0e020b92f787c6e59b7595bd2d9d2fcdd4ebd
Victor Deleau <victor.deleau@gmx.com>
2020-10-02 21:16:42 +0200

    add env config to regular backup

--- backup_regular/backup_regular.sh
+++ backup_regular/backup_regular.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# backup all partitions of a disk in a backup folder
+
 START_TIME=$SECONDS
 DATE=`date +%d%m%y`
 
-SRC=$SOURCE_DISK
-DST=$BACKUP_DISK
+# those env variables must be set
+SRC=$BACKUP_SOURCE_DISK
+DST=$BACKUP_DESTINATION_DISK
+PAR=$BACKUP_SOURCE_DISK_PARTICULE
+IDX=$BACKUP_SOURCE_DISK_START_IDX
 
 echo " "
 echo "---------------------------------------"
@@ -12,18 +17,27 @@ echo "------------- RAW backup --------------"
 echo "---------------------------------------"
 echo " "
 
-echo "Source      -> $SRC"
+echo "Source      -> /dev/$SRC"
 echo "Destination -> $DST"
 echo " "
-mkdir $DST/backup_$DATE
-sgdisk --backup=$DST/backup_$DATE/partition_table_$DATE.txt $SRC
-dd if=/dev/nvme0n1p1 of=$DST/backup_$DATE/nvme0n1p1.img status=progress bs=8092
-echo " "
-dd if=/dev/nvme0n1p2 of=$DST/backup_$DATE/nvme0n1p2.img status=progress bs=8092
-echo " "
-dd if=/dev/nvme0n1p3 of=$DST/backup_$DATE/nvme0n1p3.img status=progress bs=8092
-echo " "
+
+sudo mkdir $DST/backup_$DATE &> /dev/null
+sudo sgdisk --backup=$DST/backup_$DATE/partition_table_$DATE.txt /dev/$SRC
+
+for (( ; ; )) # loop over all partitions of the disk
+do
+    if [ -e "/dev/$SRC$PAR$IDX" ]
+    then
+        echo "Backup partition /dev/$SRC$PAR$IDX"
+        sudo dd if=/dev/$SRC$PAR$IDX of=$DST/backup_$DATE/$SRC$PAR$IDX.img status=progress bs=8092
+        IDX=$(($IDX+1))
+        echo " "
+    else
+        break
+    fi
+done
 
 ELAPSED_TIME=&(($SECONDS - $START_TIME))
+echo " "
 echo "Backup took $ELAPSED_TIME seconds"
 echo " "
--- backup_regular/readme.md
+++ backup_regular/readme.md
@@ -3,3 +3,21 @@
 In 2 steps:
 - Backup all partitions using `dd`
 - Backup gpt partition table with `sgdisk`
+
+4 environment variables are expected to be set:
+
+>$BACKUP_SOURCE_DISK
+
+Name of the disk to backup, without /dev/
+
+> $BACKUP_DESTINATION_DISK
+
+Path of the destination folder.
+
+> $BACKUP_SOURCE_DISK_PARTICULE
+
+Optional particule before the partition index. For a disk nvme0n1, and a partition nvme0n1p1, this would be p.
+
+> $BACKUP_SOURCE_DISK_START_IDX
+
+Starting index of the partitions, often 0 or 1.
